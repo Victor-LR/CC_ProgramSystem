ba203a69c2d11b303e4fc3c777414887fd44b3b5
Victor <victor.lelu_ribaimont@yahoo.fr>
2020-04-07 10:48:37 +0200

    question 1

--- cc_main.cpp
+++ cc_main.cpp
@@ -12,8 +12,48 @@ using namespace std;
 
 int main(int argc, char*argv[])
 {
-    cout << "test ?" << endl;
-    Test t;
+    int N = std::atoi(argv[1]);
+
+    const char * ligne_c;
+    string ligne_s;
+
+    char * argu[2];
+    argu[1] = NULL;
+    int compt = 0;
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        while( compt < 100) {
+           printf("MonShell> ");
+           std::cin >> ligne_s;
+          // ligne_c = ligne_s.c_str();
+
+           argu[0] = (char*) ligne_c;
+
+          // cout << ligne_s << endl;
+
+           pid_t pid_2 = fork();
+
+           if (pid_2 == 0) {
+           //   cout << pid_2 << endl;
+               execvp(ligne_c,argu);
+
+           }else
+               wait(NULL);
+
+           if (ligne_s.compare("quitter") == 0){
+               exit(0);
+           }
+
+           if (ligne_s.compare("convertirgris") == 0){
+               exit(0);
+           }
+
+          compt++;
+        }
+    }else
+        wait(NULL);
 
     return 0;
 }
