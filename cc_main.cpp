70bf336b7ab44624018846cc36eda4c10f0a1bac
Leng <jerome.leng@etud.univ-angers.fr>
2020-04-07 11:03:57 +0200

    Question 2

--- cc_main.cpp
+++ cc_main.cpp
@@ -6,13 +6,42 @@
 #include <sys/wait.h>
 #include <algorithm>
 #include <fstream>
+#include <thread>
 #include "test.cpp"
+#include "bitmap_image.hpp"
 
 using namespace std;
 
+void changegris(bitmap_image & bmp, unsigned int l1, unsigned int l2){
+    unsigned int cs2=bmp.width();
+    for (auto l=l1;l<=l2;l++) {
+        for (unsigned int c=0;c<=cs2;++c) {
+            unsigned char r1,g1,b1;
+            bmp.get_pixel(c,l,r1,g1,b1);
+            unsigned char total = (r1+g1+b1)/3;
+            bmp.set_pixel(c,l,total,total,total);
+        }
+    }
+}
+
+void convertirgris(std::string const & filename, unsigned int N){
+    bitmap_image bmp(filename);
+    std::vector<std::thread> threads;
+    for (int i = 0; i < N; ++i) {
+        unsigned int i1=i*(bmp.height()/N);
+        unsigned int i2=(i+1)*(bmp.height()/N)-1;
+        if(i == (N-1))
+            i2=bmp.height()-1;
+        threads.push_back(std::thread(changegris,std::ref(bmp),i1,i2));
+    }
+    for(auto & t : threads)
+        t.join();
+    bmp.save_image("sortie.bmp");
+}
+
 int main(int argc, char*argv[])
 {
-    cout << "test ?" << endl;
+    convertirgris("image.bmp",4);
     Test t;
 
     return 0;
